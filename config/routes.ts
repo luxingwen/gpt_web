8b51121f5e333dfeba6ab913e5b0d05903538de9
Lewis <shuaicode@gmail.com>
2023-06-14 18:10:44 +0800

    add home, ai draw api

--- config/routes.ts
+++ config/routes.ts
@@ -24,4 +24,9 @@ export default [
     path: '/smart-chat/scene',
     component: '@/pages/SmartChat/index',
   },
+  {
+    exact: true,
+    path: '/home',
+    component: '@/pages/Home/index',
+  },
 ];
