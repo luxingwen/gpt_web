0145e7bf3833a55cda3c5e974502d99673d6c1aa
Draco <draco.coder@gmail.com>
2023-06-19 11:28:08 +0800

    chore: 修复项目配置

---
+++ typings/typings.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Module: module.name
+ * @Description: your description
+ * @Author: draco
+ * @Email: draco.coder@gmail.com
+ * @Github: https://github.com/draco-china
+ * @Date: 2023-06-19 11:27:38
+ * @LastEditTime: 2023-06-19 11:27:38
+ */
+import '@umijs/max/typings';
